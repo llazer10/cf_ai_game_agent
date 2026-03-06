07a528b78da113a25b0fe9367fef0a13a9a8a808
Lazer <lazuural@gmail.com>
2026-03-06 19:34:02 +0000

    removed weather tool

--- src/server.ts
+++ src/server.ts
@@ -49,7 +49,7 @@ export class ChatAgent extends AIChatAgent<Env> {
 
     const result = streamText({
       model: workersai("@cf/zai-org/glm-4.7-flash"),
-      system: `You are an AI video game recommendation assistant.Help users discover games based on genre, platform, and personal preferences. Provide engaging and personalized recommendations to enhance their gaming experience.
+      system: `You are an AI video game recommendation assistant. elp users discover games based on genre, platform, and personal preferences. Provide engaging and personalized recommendations to enhance their gaming experience.
 
 ${getSchedulePrompt({ date: new Date() })}
 
